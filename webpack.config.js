93e3bb03664004988c4bb827fda411ca26f33620
Ziyue Chen <zchen@zobristinc.com>
2018-03-29 18:28:04 -0700

    Set up babel and react

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,16 @@ module.exports = {
 	entry: [
 		'./src/index.js'
 	],
+	module: {
+    	rules: [{
+        	test: /\.(js|jsx)$/,
+        	exclude: /node_modules/,
+        	use: ['babel-loader']
+      	}]
+  	},
+ 	resolve: {
+    	extensions: ['*', '.js', '.jsx']
+  	},
 	devtool: 'inline-source-map',
 	devServer: {
 		contentBase: './dist'
