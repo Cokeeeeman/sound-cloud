eb5c9cf498fa6f4b2cfc173ba1bbf3580c85a6c6
Ziyue Chen <zchen@zobristinc.com>
2018-03-29 17:41:32 -0700

    Set up webpack and webpack-dev-server

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+module.exports = {
+	entry: [
+		'./src/index.js'
+	],
+	devtool: 'inline-source-map',
+	devServer: {
+		contentBase: './dist'
+	},
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js'
+	}
+};
